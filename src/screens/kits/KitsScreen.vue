8c60e6f4a6c468c3d8c6defd48e4661e5dbf72ec
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2023-11-29 23:02:39 +0300

    POPPER-T-225 Верстка таблицы задач

--- src/screens/kits/KitsScreen.vue
+++ src/screens/kits/KitsScreen.vue
@@ -3,13 +3,7 @@
         <n-h1>Наборы</n-h1>
 
         <div class="kit-content">
-            <n-space vertical :size="12">
-    <n-data-table
-      :columns="columns"
-      :data="data"
-      :pagination="pagination"
-    />
-  </n-space>
+            <n-data-table :columns="columns" :data="data" :scroll-x="1000" style="height: 100%" flex-height />
         </div>
     </div>
 </template>
@@ -31,85 +25,101 @@ import axios from 'axios';
 
 
 const createColumns = ({
-  sendMail
+    sendMail
 }) => {
-  return [
-    {
-      title: "Номер",
-      key: "kit_number"
-    },
-    {
-      title: "ТЗ",
-      key: "specification_title"
-    },
-    {
-      title: "Кол-во",
-      key: "kit_size"
-    },
-    {
-      title: "В работе",
-      key: "products_in_work"
-    },
-    {
-      title: "Готово",
-      key: "products_done"
-    },
-    {
-      title: "Прошло ОТК",
-      key: "control_progress.OTK"
-    },
-    {
-      title: "Прошло Испытания",
-      key: "control_progress.TESTING"
-    },
-    {
-      title: "Заблокировано",
-      key: "locked_quantity"
-    },
-    {
-      title: "Забраковано",
-      key: "defected_quantity"
-    },
-    {
-      title: "",
-      key: "delete",
-      render(row) {
-        return h(
-          NButton,
-          {
-            size: "small",
-            onClick: () => sendMail(row)
-          },
-          { default: () => "Send Email" },
-        );
-      }
-    }
-  ];
+    return [
+        {
+            title: "Номер",
+            key: "kit_number",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "ТЗ",
+            key: "specification_title",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Кол-во",
+            key: "kit_size",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "В работе",
+            key: "products_in_work",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Готово",
+            key: "products_done",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Прошло ОТК",
+            key: "control_progress.OTK",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Прошло Испытания",
+            key: "control_progress.TESTING",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Заблокировано",
+            key: "locked_quantity",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "Забраковано",
+            key: "defected_quantity",
+            align: 'center',
+            width: 100
+        },
+        {
+            title: "",
+            key: "delete",
+            render(row) {
+                return h(
+                    NButton,
+                    {
+                        size: "small",
+                        onClick: () => sendMail(row)
+                    },
+                    { default: () => "Send Email" },
+                );
+            },
+            align: 'center',
+            width: 100
+        }
+    ];
 };
 
 
 
 export default defineComponent({
-  setup() {
-    const message = useMessage();
-    const kits = ref([]);
-    onMounted(async () => {
-      const res = await axios.get("https://test.remcoil.webtm.ru/kit/progress");
-      kits.value = res.data;
-      console.log(res);
-    });
+    setup() {
+        const message = useMessage();
+        const kits = ref([]);
+        onMounted(async () => {
+            const res = await axios.get("https://test.remcoil.webtm.ru/kit/progress");
+            kits.value = res.data;
+        });
 
-    return {
-      data: kits,
-      columns: createColumns({
-        sendMail(rowData) {
-          message.info("send mail to " + rowData.products_done);
-        }
-      }),
-      pagination: {
-        pageSize: 10
-      }
-    };
-  }
+        return {
+            data: kits,
+            columns: createColumns({
+                sendMail(rowData) {
+                    message.info("send mail to " + rowData.products_done);
+                }
+            })
+        };
+    }
 });
 </script>
